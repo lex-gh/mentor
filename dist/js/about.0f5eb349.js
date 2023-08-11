5d1c541f4866ed7e18c65bf19456e41627e636e0
abra-93 <maskall93@gmail.com>
2023-08-11 17:00:25 +0300

    Adding dist

---
+++ dist/js/about.0f5eb349.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmentor"]=self["webpackChunkmentor"]||[]).push([[443],{6298:function(n,t,e){e.r(t),e.d(t,{default:function(){return l}});var r=e(3396);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),s=[a];function c(n,t){return(0,r.wg)(),(0,r.iD)("div",u,s)}var o=e(89);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.0f5eb349.js.map
